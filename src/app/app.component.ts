089265ce45ac05ab3ac529a9bce69ec1a39ba9c0
gahbnahs <shansgautam@outlook.com>
2019-02-19 22:36:30 -0500

    updated title, first commit to branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  pageTitle = 'product-management';
+  pageTitle = 'Manage Products';
 }
